1244d9b109ef80cce5607160b2d024438cb63a93
Michael Valdez <mikeyvaldez92@yahoo.com>
2022-11-23 22:54:18 -0800

    defanging an IP Adress

---
+++ easy/strings/defangingAnIPAdreess.js
@@ -0,0 +1,37 @@
+
+// Defanging an IP Address
+
+
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+// A defanged IP address replaces every period "." with "[.]".
+
+
+
+// Example 1:
+
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+// Example 2:
+
+// Input: address = "255.100.50.0"
+// Output: "255[.]100[.]50[.]0"
+
+
+
+const defangIPaddr = function(address) {
+  let newIPaddr = address.split("")
+
+  for(let i = 0; i < newIPaddr.length; i++){
+    if(newIPaddr[i] === '.'){
+      newIPaddr[i] = '[.]'
+    }
+  }
+  return newIPaddr.join('')
+}
+
+
+
+
+console.log(defangIPaddr("1.1.1.1"))
+console.log(defangIPaddr("255.100.50.0"))
