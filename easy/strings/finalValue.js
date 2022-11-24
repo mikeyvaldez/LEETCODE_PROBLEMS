ba7c357f73a02a10ab2c91718911eb2fa2b0621f
Michael Valdez <mikeyvaldez92@yahoo.com>
2022-11-23 23:24:54 -0800

    finalValue

---
+++ easy/strings/finalValue.js
@@ -0,0 +1,60 @@
+
+// There is a programming language with only four operations and one variable X:
+
+// ++X and X++ increments the value of the variable X by 1.
+// --X and X-- decrements the value of the variable X by 1.
+// Initially, the value of X is 0.
+
+// Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+const finalValueAfterOperations = function(operations){
+  let counter = 0;
+  for(let i = 0; i < operations.length; i++){
+    if(operations[i].includes('--')){
+      counter--;
+    } else if(operations[i].includes('++')){
+      counter++;
+    }
+  }
+  return counter;
+}
+
+
+// Example 1:
+
+// Input: operations = ["--X","X++","X++"]
+// Output: 1
+// Explanation: The operations are performed as follows:
+// Initially, X = 0.
+// --X: X is decremented by 1, X =  0 - 1 = -1.
+// X++: X is incremented by 1, X = -1 + 1 =  0.
+// X++: X is incremented by 1, X =  0 + 1 =  1.
+console.log(finalValueAfterOperations(["--X","X++","X++"]))
+
+// Example 2:
+
+// Input: operations = ["++X","++X","X++"]
+// Output: 3
+// Explanation: The operations are performed as follows:
+// Initially, X = 0.
+// ++X: X is incremented by 1, X = 0 + 1 = 1.
+// ++X: X is incremented by 1, X = 1 + 1 = 2.
+// X++: X is incremented by 1, X = 2 + 1 = 3.
+console.log(finalValueAfterOperations(["++X","++X","X++"]))
+
+// Example 3:
+
+// Input: operations = ["X++","++X","--X","X--"]
+// Output: 0
+// Explanation: The operations are performed as follows:
+// Initially, X = 0.
+// X++: X is incremented by 1, X = 0 + 1 = 1.
+// ++X: X is incremented by 1, X = 1 + 1 = 2.
+// --X: X is decremented by 1, X = 2 - 1 = 1.
+// X--: X is decremented by 1, X = 1 - 1 = 0.
+console.log(finalValueAfterOperations(["X++","++X","--X","X--"]))
+
+// Constraints:
+
+// 1 <= operations.length <= 100
+// operations[i] will be either "++X", "X++", "--X", or "X--".
